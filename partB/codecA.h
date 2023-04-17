eb080a03f60b577502b1f44f7c22360395eb5d93
roey-ubuntu <roey.fenigold@gmail.com>
2023-04-17 14:45:22 +0300

    create codecA.h

---
+++ partB/codecA.h
@@ -0,0 +1,18 @@
+#ifndef CODEC_A_H
+#define CODEC_A_H
+
+/**
+ * Convert all lower case chars to upper case, and all upper case to lower case. All other chars will remain unchanged.
+ * @param str String to encode.
+ */
+void encode(char *str);
+
+/**
+ * Decode the string back to it's original form.
+ * @param str - String to decode.
+ */
+void decode(char *str);
+
+#endif
+
+
