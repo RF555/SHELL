7b4f11100192421be4532dc017e3d8a11445115e
roey-ubuntu <roey.fenigold@gmail.com>
2023-04-17 14:45:50 +0300

    create codecB.h

---
+++ partB/codecB.h
@@ -0,0 +1,16 @@
+#ifndef CODEC_B_H
+#define CODEC_B_H
+
+/**
+ * Convert all chars to the 3-rd next char (adding a number of 3 to the ASCII value).
+ * @param str String to encode.
+ */
+void encode(char *str);
+
+/**
+ * Decode the string back to it's original form.
+ * @param str - String to decode.
+ */
+void decode(char *str);
+
+#endif
