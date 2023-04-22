07ca45b3727da0835715c9fd7832ea3acd1a05cc
RF555 <roey.feingold@gmail.com>
2023-04-22 15:50:25 +0300

    update basic_stshell.c

--- basic_stshell.c
+++ basic_stshell.c
@@ -8,35 +8,34 @@
 #include <string.h>
 
 int main() {
-	int i;
-	char *argv[10];
-	char command[1024];
-	char *token;
+    int i;
+    char *argv[10];
+    char command[1024];
+    char *token;
 
-	while (1) {
-	    printf("hello: ");
-	    fgets(command, 1024, stdin);
-	    command[strlen(command) - 1] = '\0'; // replace \n with \0
+    while (1) {
+        printf("hello: ");
+        fgets(command, 1024, stdin);
+        command[strlen(command) - 1] = '\0'; // replace \n with \0
 
-	    /* parse command line */
-	    i = 0;
-	    token = strtok (command," ");
-	    while (token != NULL)
-	    {
-		argv[i] = token;
-		token = strtok (NULL, " ");
-		i++;
-	    }
-	    argv[i] = NULL;
+        /* parse command line */
+        i = 0;
+        token = strtok(command, " ");
+        while (token != NULL) {
+            argv[i] = token;
+            token = strtok(NULL, " ");
+            i++;
+        }
+        argv[i] = NULL;
 
-	    /* Is command empty */
-	    if (argv[0] == NULL)
-		continue;
+        /* Is command empty */
+        if (argv[0] == NULL)
+            continue;
 
-	    /* for commands not part of the shell command language */
-	    if (fork() == 0) {
-		execvp(argv[0], argv);
-		wait(NULL);
-	    }
-	}
+        /* for commands not part of the shell command language */
+        if (fork() == 0) {
+            execvp(argv[0], argv);
+            wait(NULL);
+        }
+    }
 }
