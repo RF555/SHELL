bd440ff311ac796554f79daeb7193bbb54f23ff0
RF555 <roey.feingold@gmail.com>
2023-04-23 18:02:17 +0300

    update

--- basic_stshell.c
+++ basic_stshell.c
@@ -7,24 +7,34 @@
 #include "unistd.h"
 #include <string.h>
 
+#include "my_stshell.h"
+
+
+int MAX_INPUT_LEN = 1024;
+enum ALIVE is_alive = TRUE;
+
 int main() {
     int i;
     char *argv[10];
     char command[1024];
     char *token;
 
-    while (1) {
-        printf("hello: ");
-        fgets(command, 1024, stdin);
+    int line=0;
+
+    while (is_alive) {
+        /* Start of shell. */
+        printf("%d hello: ",line);
+
+        /* Get input from user and store it in `command`. */
+        fgets(command, MAX_INPUT_LEN, stdin);
         command[strlen(command) - 1] = '\0'; // replace \n with \0
 
         /* parse command line */
         i = 0;
-        token = strtok (command," ");
-        while (token != NULL)
-        {
+        token = strtok(command, " ");
+        while (token != NULL) {
             argv[i] = token;
-            token = strtok (NULL, " ");
+            token = strtok(NULL, " ");
             i++;
         }
         argv[i] = NULL;
---
+++ my_stshell.h
@@ -0,0 +1,9 @@
+#ifndef MY_STSHELL_H
+#define MY_STSHELL_H
+
+enum ALIVE {
+    TRUE = 1,
+    FALSE = 0
+};
+
+#endif //MY_STSHELL_H
