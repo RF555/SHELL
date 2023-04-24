53b3ba88e5a943091f72cd2842359234adbca6f7
RF555 <roey.feingold@gmail.com>
2023-04-24 15:31:10 +0300

    update - need to check <, >, >>, exit, and CMD tools (all PIPE code is in comment)

--- stshell.c
+++ stshell.c
@@ -14,19 +14,20 @@ enum ALIVE {
     FALSE = 0
 };
 
+int MAX_INPUT_LEN = 1024;
+int WAIT = 1;
+enum ALIVE is_alive = TRUE;
+
 /**
  * Signal handler function for SIGINT (Ctrl+C).
  */
-void handle_sigint() {
+void handle_sigint(void) {
     printf("\n"); // Print newline after Ctrl+C
-    signal(SIGINT, handle_sigint); // Reinstall the signal handler
+    signal(SIGINT, (__sighandler_t) handle_sigint); // Reinstall the signal handler
 }
 
 
-int MAX_INPUT_LEN = 1024;
-enum ALIVE is_alive = TRUE;
-
-int main() {
+int main(void) {
     int i;
     char *argv[10];
     char command[MAX_INPUT_LEN];
@@ -36,15 +37,18 @@ int main() {
     char *input_file;
     int output_file_descriptor = -1;
     int input_file_descriptor = -1;
+//    int pipes[2][2]; // Array of pipe file descriptors
+//    int num_pipes; // Number of pipes to be created
     pid_t pid;
 
-    int line = 0;
+//    int line = 0;
 
-    signal(SIGINT, handle_sigint); // Register signal handler for Ctrl+C
+    signal(SIGINT, (__sighandler_t) handle_sigint); // Register signal handler for Ctrl+C
 
     while (is_alive) {
         /* Start of shell */
-        printf("%d hello: ", line++);
+//        printf("%d hello: ", line++);
+        printf("hello: ");
         fflush(stdout);
 
         /* Get input from user and store it in `command` */
@@ -57,20 +61,15 @@ int main() {
         /* parse command line */
         i = 0;
         while (token != NULL) {
-            /* Redirection OUTPUT by `>` OR `>>` */
-            if (strcmp(token, ">") == 0 || strcmp(token, ">>") == 0) {
+            /* Redirect OUTPUT by `>` */
+            if (strcmp(token, ">") == 0) {
                 /* Get the output file path */
                 output_file = strtok(NULL, " ");
                 if (output_file == NULL) {
                     fprintf(stderr, "Error: missing output file after %s\n", token);
                     break;
                 }
-                /* Distinguish between `>` and `>>` */
-                if (strcmp(token, ">") == 0) {
-                    output_file_descriptor = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-                } else {
-                    output_file_descriptor = open(output_file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-                }
+                output_file_descriptor = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
                 if (output_file_descriptor < 0) {
                     perror("open");
                     break;
@@ -80,7 +79,24 @@ int main() {
                 close(output_file_descriptor);
                 break;
 
-                /* Redirection INPUT by `<` */
+                /* Redirect OUTPUT by `>>` */
+            } else if (strcmp(token, ">>") == 0) {
+                output_file = strtok(NULL, " ");
+                if (output_file == NULL) {
+                    fprintf(stderr, "Error: missing output file after %s\n", token);
+                    break;
+                }
+                output_file_descriptor = open(output_file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+                if (output_file_descriptor < 0) {
+                    perror("open");
+                    break;
+                }
+                /* Redirect the standard output (stdout) to the file descriptor */
+                dup2(output_file_descriptor, STDOUT_FILENO);
+                close(output_file_descriptor);
+                break;
+
+                /* Redirect INPUT by `<` */
             } else if (strcmp(token, "<") == 0) {
                 input_file = strtok(NULL, " ");
                 if (input_file == NULL) {
@@ -97,8 +113,11 @@ int main() {
                 break;
 
 
-
                 /* Handle piping */
+            } else if (strcmp(token, "|") == 0) {
+                argv[i] = NULL;
+
+
 
 
 //            } else if (strcmp(token, "|") == 0) {
@@ -106,7 +125,8 @@ int main() {
 //                    perror("pipe");
 //                    break;
 //                }
-//                if ((pid = fork()) == -1) {
+//                pid = fork();
+//                if (pid == -1) {
 //                    perror("fork");
 //                    break;
 //                } else if (pid == 0) { // Child process
@@ -128,7 +148,7 @@ int main() {
 //                    while (token != NULL) {
 //                        argv[i] = token;
 //                        token = strtok(NULL, " ");
-//                        i++;
+//                        ++i;
 //                    }
 //                    argv[i] = NULL;
 //                    if (i == 0) {
@@ -144,11 +164,10 @@ int main() {
 //                }
 
 
-
             } else { // Regular command
                 argv[i] = token;
                 token = strtok(NULL, " ");
-                i++;
+                ++i;
             }
         }
         argv[i] = NULL;
@@ -157,7 +176,6 @@ int main() {
         if (argv[0] == NULL) {
             continue;
         }
-
 
         /* Exit command */
         if (strcmp(argv[0], "exit") == 0) {
@@ -169,12 +187,12 @@ int main() {
         pid = fork();
         if (pid == -1) {
             perror("Fork ERROR - can't get process ID");
-        } else if (pid == 0) {
+        } else if (pid == 0) { /* Child process */
 //            printf("(fork) Executing command: %s\n", argv[0]);
             execvp(argv[0], argv);
 //            perror("Error executing command");
             exit(1);
-        } else {
+        } else { /* Parent process */
             wait(NULL);
 //            printf("Command %s executed\n", argv[0]);
         }
